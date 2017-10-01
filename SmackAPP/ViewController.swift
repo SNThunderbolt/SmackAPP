41f23d71470cb622cd19d043147c5f98fb86b5d4
SNThunderbolt <loadstar.saji@gmail.com>
2017-10-01 09:20:31 +0330

    Initial Commit

---
+++ SmackAPP/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SmackAPP
+//
+//  Created by SajedeNouri on 7/9/1396 AP.
+//  Copyright © 1396 AP SajedeNouri. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
