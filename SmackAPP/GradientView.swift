2d0eae5b68745ba699774ce20c4112b90abf0a67
SNThunderbolt <loadstar.saji@gmail.com>
2017-10-01 20:54:53 +0330

    UIView Design developed a bit..

---
+++ SmackAPP/GradientView.swift
@@ -0,0 +1,21 @@
+//
+//  GradientView.swift
+//  SmackAPP
+//
+//  Created by SajedeNouri on 7/9/1396 AP.
+//  Copyright © 1396 AP SajedeNouri. All rights reserved.
+//
+
+import UIKit
+
+class GradientView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
